17c107480773a88c8fcf430d64a2cc12be09d359
pyta <piociw@gmail.com>
2015-04-02 18:12:47 +0200

    Dodanie projektu GiveItBack.

---
+++ GiveItBack/GiveItBack/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GiveItBack.Resources;
+
+namespace GiveItBack
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
